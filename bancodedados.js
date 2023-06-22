fb6dbc66dea35defcd3096f2038ba139d581fc22
anamariagds <gomesabam@gmail.com>
2023-06-22 13:01:09 -0300

    para corrigir erro no render

--- bancodedados.js
+++ bancodedados.js
@@ -5,7 +5,7 @@ async function conectaBd() {
     try {
         console.log('Conexão com banco de dados iniciada...')
 
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
 
         console.log('Conexão feita com sucesso!')
     } catch(erro){
